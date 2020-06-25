63c91006f1e1e6594fb78dddbc63fb3773ad21a3
sosualfred <sosualfred@gmail.com>
2020-06-25 21:43:19 +0000

    Implemented Basic CRUD on Notofication model

--- laravelapi/app/Http/Controllers/NotificationController.php
+++ laravelapi/app/Http/Controllers/NotificationController.php
@@ -30,15 +30,65 @@ class NotificationController extends Controller
     }
 
     public function getNotification($id) {
-    // logic to get a Notification record goes here
+        // logic to get a Notification record goes here
+
+        if (Notification::where('id', $id)->exists()) {
+
+            $notification = Notification::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($notification, 200);
+
+        } else {
+            return response()->json([
+                "message" => "Notification not found"
+            ], 404);
+        }
+
     }
 
     public function updateNotification(Request $request, $id) {
-    // logic to update a Notification record goes here
+
+        // logic to update a Notification record goes here
+        if (Notification::where('id', $id)->exists()) {
+
+            $notification = Notification::find($id);
+
+            $notification->title = is_null($request->title) ? $notification->title : $request->title;
+            $notification->body = is_null($request->body) ? $notification->body : $request->body;
+            $notification->icon = is_null($request->icon) ? $notification->icon : $request->icon;
+
+            $notification->save();
+
+            return response()->json([
+                "message" => "Notification records updated successfully"
+            ], 200);
+
+        } else {
+            return response()->json([
+                "message" => "Notification not found"
+            ], 404);
+        }
+
     }
 
     public function deleteNotification ($id) {
-    // logic to delete a Notification record goes here
+        // logic to delete a Notification record goes here
+
+        if (Notification::where('id', $id)->exists()) {
+
+        $notification = Notification::find($id);
+
+        $notification->delete();
+
+        return response()->json([
+            "message" => "Notification record deleted"
+        ], 202);
+
+        } else {
+        return response()->json([
+            "message" => "Notification not found"
+        ], 404);
+        }
+
     }
 
 
