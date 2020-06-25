f895709325e5279a29682d62f39ac14f3b29d3c1
sosualfred <sosualfred@gmail.com>
2020-06-25 20:22:04 +0000

    create routes for adding and listing notifications

--- laravelapi/routes/api.php
+++ laravelapi/routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::get('notifications', 'NotificationController@getAllNotifications');
+Route::get('notifications/{id}', 'NotificationController@getNotification');
+Route::post('notifications', 'NotificationController@createNotification');
+Route::put('notifications/{id}', 'NotificationController@updateNotification');
+Route::delete('notifications/{id}','NotificationController@deleteNotification');
