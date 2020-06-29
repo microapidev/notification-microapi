f557b5904e54fc8bf64744ef2bf3b2b1a62e6508
Botus <abiolatitus@hotmail.com>
2020-06-29 15:59:37 +0100

    Send notifications to users

--- laravelapi/app/Http/Controllers/SendController.php
+++ laravelapi/app/Http/Controllers/SendController.php
@@ -13,5 +13,16 @@ class SendController extends Controller
 
     public function sendNotification(Request $request){
         // Write your code here for this endpoint
+        
+        // send email to subscribers
+        
+        $subscribers = Subscriber::all(); //Retrieving all subscribers
+ 
+        foreach($subscribers as $subscriber){
+            Notification::route('mail' , $subscriber->email) //Sending mail to subscriber
+                          ->notify(new NewPostNotify($posts)); //With new post
+ 
+        return redirect()->back();
+      }
     }
 }
